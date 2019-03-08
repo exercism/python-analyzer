b6a897d5c12c944198b6781030fb7b3999018399
Trey42 <tea033098@gmail.com>
2019-03-07 19:49:59 -0500

    Analyzer now checks for several more conditions to give feedback on. All conditions currently checked for fully tested.

--- analyzer.py
+++ analyzer.py
@@ -6,6 +6,12 @@ var_convention = "It is Python convention to name variables using underscores an
                  "mentors."
 no_method = "No method called two_fer."
 malformed_code = "The code is malformed and cannot be parsed for analysis."
+simple_concat = "String concatenation with the + operator is a valid approach, but f-strings and str.format offer more " \
+                "functionality and elegant solutions."
+no_def_arg = "No default arguments are used in this solution. An ideal solution should make use of a default argument " \
+             "and either f-strings or str.format."
+conditionals = "Conditionals are unnecessarily used in this solution. An ideal solution should make use of a default " \
+               "argument and either f-strings or str.format."
 
 def analyze(user_solution):
     """
@@ -23,16 +29,21 @@ def analyze(user_solution):
 
     #List of comments to return at end, each comment is a string
     comments = []
-    #Whether to approve the user's solution based on analysis
+    #Whether to approve the user's solution based on analysis. Note that this only denotes if it's POSSIBLE for the
+    #user's solution to be approved; just because the user didn't submit something that automatically makes it get
+    #disapproved, like an empty file or missing method header, doesn't mean it's actually correct. Final assessment
+    #of the user's solution must be done by a mentor.
     approve = True
     #Does the solution have a method called two_fer?
     has_method = False
+    #Does the solution correctly use a default argument?
+    uses_def_arg = False
 
     for node in ast.walk(tree):
         #Find all variable names in the user's code to check for naming conventions
         if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store):
             for char in node.id:
-                if char.isupper(): comments += [var_convention]
+                if char.isupper() and var_convention not in comments: comments += [var_convention]
 
         #Find all attribute names in the user's code to check for naming convetions again
         if isinstance(node, ast.Attribute):
@@ -43,8 +54,21 @@ def analyze(user_solution):
         if isinstance(node, ast.FunctionDef):
             if node.name == 'two_fer': has_method = True
 
+        #Search for use of string concatenation with + operator
+        if isinstance(node, ast.Add) and simple_concat not in comments: comments += [simple_concat]
+
+        #Search for use of default arguments
+        if isinstance(node, ast.arguments):
+            if node.defaults: uses_def_arg = True
+
+        #Search for use of unnecessary conditionals
+        if isinstance(node, ast.If) and conditionals not in comments: comments += [conditionals]
+
     if not has_method:
         comments += [no_method]
         approve = False
+
+    if not uses_def_arg:
+        comments += [no_def_arg]
 
     return (comments, approve)
--- two_fer_test.py
+++ two_fer_test.py
@@ -2,6 +2,14 @@ import ast
 import unittest
 import analyzer
 
+#This is only here because the indentation within the class and function definitions makes ast.parse fail
+#for multi-line function definitions in string form
+conditional = '''
+def two_fer(name=None):
+    if not name: return "One for you, one for me."
+    else: return "One for %s, one for me." %name
+'''
+
 class TwoFerTest(unittest.TestCase):
     def test_camel_case(self):
         camel_case = "someVar = 0"
@@ -17,16 +25,53 @@ class TwoFerTest(unittest.TestCase):
         malformed_input = ")^*()&*$@Jfjlkajsdf;"
         feedback = analyzer.analyze(malformed_input)
         self.assertTrue(analyzer.malformed_code in feedback[0])
+        self.assertFalse(feedback[1])
 
     def test_missing_method(self):
         wrong_method_name = '''def one_fer(name="you"): return "One for %s, one for me." % name'''
         feedback = analyzer.analyze(wrong_method_name)
         self.assertTrue(analyzer.no_method in feedback[0])
+        self.assertFalse(feedback[1])
 
     def test_has_method(self):
         correct_method_name = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
         feedback = analyzer.analyze(correct_method_name)
         self.assertFalse(analyzer.no_method in feedback[0])
+
+    def test_simple_concat(self):
+        simple_concat = '''def two_fer(name="you"): return "One for " + name + ", one for me."'''
+        feedback = analyzer.analyze(simple_concat)
+        self.assertTrue(analyzer.simple_concat in feedback[0])
+
+    def test_no_simple_concat(self):
+        correct_style = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
+        feedback = analyzer.analyze(correct_style)
+        self.assertFalse(analyzer.simple_concat in feedback[0])
+
+    def test_approves_correct_solution(self):
+        correct_style = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
+        feedback = analyzer.analyze(correct_style)
+        self.assertTrue(feedback[1])
+        self.assertFalse(feedback[0])
+
+    def test_no_def_arg(self):
+        no_def_arg = '''def two_fer(name): return "One for %s, one for me." % name'''
+        feedback = analyzer.analyze(no_def_arg)
+        self.assertTrue(analyzer.no_def_arg in feedback[0])
+
+    def test_uses_def_args(self):
+        def_arg = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
+        feedback = analyzer.analyze(def_arg)
+        self.assertFalse(analyzer.no_def_arg in feedback[0])
+
+    def test_uses_conditionals(self):
+        feedback = analyzer.analyze(conditional)
+        self.assertTrue(analyzer.conditionals in feedback[0])
+
+    def test_no_conditionals(self):
+        correct_style = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
+        feedback = analyzer.analyze(correct_style)
+        self.assertFalse(analyzer.conditionals in feedback[0])
 
 if __name__ == '__main__':
     unittest.main()
