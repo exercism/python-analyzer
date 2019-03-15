30c76c58277c0e5392bdee6b555dc8157119e872
Xitang <xz522@cornell.edu>
2019-03-15 09:31:24 -0400

    Change test case input to file

--- two_fer_test.py
+++ two_fer_test.py
@@ -1,6 +1,7 @@
 import ast
 import unittest
 import analyzer
+import os
 
 #This is only here because the indentation within the class and function definitions makes ast.parse fail
 #for multi-line function definitions in string form
@@ -10,68 +11,92 @@ def two_fer(name=None):
     else: return "One for %s, one for me." %name
 '''
 
+test_file = 'test.py'
+
+def create_test_file(user_solution):
+    file = open(test_file,  'w')
+    file.write(user_solution)
+    file.close()
+
 class TwoFerTest(unittest.TestCase):
+    def test_no_module(self):
+        feedback = analyzer.analyze('non_exist_test_file.py')
+        self.assertTrue(analyzer.no_module in feedback[0])
+        self.assertFalse(feedback[1])
+
     def test_malformed_input(self):
         malformed_input = ")^*()&*$@Jfjlkajsdf;"
-        feedback = analyzer.analyze(malformed_input)
+        create_test_file(malformed_input)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.malformed_code in feedback[0])
         self.assertFalse(feedback[1])
 
     def test_missing_method(self):
         wrong_method_name = '''def one_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(wrong_method_name)
+        create_test_file(wrong_method_name)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.no_method in feedback[0])
         self.assertFalse(feedback[1])
 
     def test_has_method(self):
         correct_method_name = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(correct_method_name)
+        create_test_file(correct_method_name)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.no_method in feedback[0])
 
     def test_simple_concat(self):
         simple_concat = '''def two_fer(name="you"): return "One for " + name + ", one for me."'''
-        feedback = analyzer.analyze(simple_concat)
+        create_test_file(simple_concat)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.simple_concat in feedback[0])
 
     def test_no_simple_concat(self):
         correct_style = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(correct_style)
+        create_test_file(correct_style)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.simple_concat in feedback[0])
 
     def test_approves_correct_solution(self):
         correct_style = '''def two_fer(name='you'): return 'One for {}, one for me.'.format(name)'''
-        feedback = analyzer.analyze(correct_style)
+        create_test_file(correct_style)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(feedback[1])
         self.assertFalse(feedback[0])
 
     def test_no_def_arg(self):
         no_def_arg = '''def two_fer(name): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(no_def_arg)
+        create_test_file(no_def_arg)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.no_def_arg in feedback[0])
 
     def test_uses_def_args(self):
         def_arg = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(def_arg)
+        create_test_file(def_arg)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.no_def_arg in feedback[0])
 
     def test_uses_conditionals(self):
-        feedback = analyzer.analyze(conditional)
+        create_test_file(conditional)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.conditionals in feedback[0])
 
     def test_no_conditionals(self):
         correct_style = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(correct_style)
+        create_test_file(correct_style)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.conditionals in feedback[0])
 
     def test_no_return(self):
         no_return = '''def two_fer(name="you"): print ("One for %s, one for me." % name)'''
-        feedback = analyzer.analyze(no_return)
+        create_test_file(no_return)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.no_return in feedback[0])
         self.assertFalse(feedback[1])
 
     def test_has_return(self):
         has_return = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(has_return)
+        create_test_file(has_return)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.no_return in feedback[0])
         self.assertTrue(feedback[1])
 
@@ -82,23 +107,29 @@ class TwoFerTest(unittest.TestCase):
 
     def test_wrong_def_arg(self):
         wrong_def_arg = '''def two_fer(name="them"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(wrong_def_arg)
+        create_test_file(wrong_def_arg)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.wrong_def_arg in feedback[0])
 
     def test_correct_def_arg(self):
         correct_def_arg = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(correct_def_arg)
+        create_test_file(correct_def_arg)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.wrong_def_arg in feedback[0])
 
     def test_uses_percent(self):
         uses_percent = '''def two_fer(name="you"): return "One for %s, one for me." % name'''
-        feedback = analyzer.analyze(uses_percent)
+        create_test_file(uses_percent)
+        feedback = analyzer.analyze(test_file)
         self.assertTrue(analyzer.percent_formatting in feedback [0])
 
     def test_no_percent(self):
         no_percent = '''def two_fer(name='you'): return 'One for {}, one for me.'.format(name)'''
-        feedback = analyzer.analyze(no_percent)
+        create_test_file(no_percent)
+        feedback = analyzer.analyze(test_file)
         self.assertFalse(analyzer.percent_formatting in feedback[0])
 
 if __name__ == '__main__':
     unittest.main()
+    os.remove(test_file)
+    print("remove")
