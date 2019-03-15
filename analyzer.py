2b7d60a45a3ae8e2c1e8c0af87f3123f04adcc48
Trey42 <tea033098@gmail.com>
2019-03-15 13:00:17 -0400

    Check for use of str.format and f-strings; fixed output when code is malformed or file can't be found

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,7 @@ from pylint import epylint as lint
 from enum import Enum
 import json
 
-#Feedback for common mistakes
+# Feedback for common mistakes
 no_module = "No module called two_fer.py found in file path."
 no_method = "No method called two_fer."
 malformed_code = "The code is malformed and cannot be parsed for analysis."
@@ -29,61 +29,92 @@ def analyze(file_path):
         entry.
     """
 
-    #Input file
+    # Input file
     try:
         with open(file_path, 'r') as f:
            user_solution = f.read()
     except:
-        return([no_module], False, [])
+        # If the proper file cannot be found, disapprove this solution
+        output = {}
+        output['status'] = 'disapprove_with_comment'
+        output['comment'] = [no_module]
+        output['pylint_comment'] = []
+        json_output = json.dumps(output)
+        file = open('analysis.json', 'w')
+        file.write(json_output)
+        file.close()
+        return ([no_module], False, [], 'disapprove_with_comment')
 
-    #Parse file to abstract syntax tree
     try:
+        # Parse file to abstract syntax tree
         tree = ast.parse(user_solution)
     except:
-        #If ast.parse fails, the code is malformed
-        return ([malformed_code], False, [])
+        # If ast.parse fails, the code is malformed and this solution is disapproved
+        output = {}
+        output['status'] = 'disapprove_with_comment'
+        output['comment'] = [malformed_code]
+        output['pylint_comment'] = []
+        json_output = json.dumps(output)
+        file = open('analysis.json', 'w')
+        file.write(json_output)
+        file.close()
+        return ([malformed_code], False, [], 'disapprove_with_comment')
 
-    #List of comments to return at end, each comment is a string
+    # List of comments to return at end, each comment is a string
     comments = []
     pylint_comments =[]
-    #Whether to approve the user's solution based on analysis. Note that this only denotes if it's POSSIBLE for the
-    #user's solution to be approved; just because the user didn't submit something that automatically makes it get
-    #disapproved, like an empty file or missing method header, doesn't mean it's actually correct. Final assessment
-    #of the user's solution must be done by a mentor.
+    # Whether to approve the user's solution based on analysis. Note that this only denotes if it's POSSIBLE for the
+    # user's solution to be approved; just because the user didn't submit something that automatically makes it get
+    # disapproved, like an empty file or missing method header, doesn't mean it's actually correct. Final assessment
+    # of the user's solution must be done by a mentor.
     approve = True
-    #Does the solution have a method called two_fer?
+    # Does the solution have a method called two_fer?
     has_method = False
-    #Does the solution correctly use a default argument?
+    # Does the solution correctly use a default argument?
     uses_def_arg = False
-    #Does the solution have a return value?
+    # Does the solution have a return value?
     has_return = False
+    # Does the solution use str.format?
+    uses_format = False
+    # Does the solution use f-strings?
+    uses_f_string = False
 
     for node in ast.walk(tree):
-        #Search for method called two_fer
+        # Search for method called two_fer
         if isinstance(node, ast.FunctionDef):
             if node.name == 'two_fer': has_method = True
 
-        #Search for use of string concatenation with + operator
+        # Search for use of string concatenation with + operator
         elif isinstance(node, ast.Add) and simple_concat not in comments: comments += [simple_concat]
 
-        #Search for use of default arguments
+        # Search for use of default arguments
         elif isinstance(node, ast.arguments):
             if node.defaults:
                 uses_def_arg = True
-                #Search for use of incorrect default argument
+                # Search for use of incorrect default argument
                 try:
                     if node.defaults[0].s != 'you' and wrong_def_arg not in comments: comments += [wrong_def_arg]
                 except:
                     if wrong_def_arg not in comments: comments += [wrong_def_arg]
 
-        #Search for use of unnecessary conditionals
+        # Search for use of unnecessary conditionals
         elif isinstance(node, ast.If) and conditionals not in comments: comments += [conditionals]
 
-        #Search for use of %-formatting
+        # Search for use of %-formatting
         elif isinstance(node, ast.Mod) and percent_formatting not in comments: comments += [percent_formatting]
 
-        #Search for return
+        # Search for return
         elif isinstance(node, ast.Return): has_return = True
+
+        # Search for use of str.format
+        elif isinstance(node, ast.Call):
+            try:
+                if node.func.attr == 'format': uses_format = True
+            except:
+                pass
+
+        # Search for use of f-strings
+        elif isinstance(node, ast.FormattedValue): uses_f_string = True
 
     if not has_method:
         comments += [no_method]
@@ -101,8 +132,8 @@ def analyze(file_path):
     pylint_comments += [pylint_stdout.getvalue()]
 
     # Set solution status
-    if (approve and not comments): status = 'approve_as_optimal'
-    elif (not approve): status = 'disapprove_with_comment'
+    if approve and (not comments) and (uses_format or uses_f_string): status = 'approve_as_optimal'
+    elif not approve: status = 'disapprove_with_comment'
     else: status = 'refer_to_mentor'
 
     # Convert to json output format and print to analysis.json
@@ -114,6 +145,5 @@ def analyze(file_path):
     file = open('analysis.json',  'w')
     file.write(json_output)
     file.close()
-
 
     return (comments, approve, pylint_comments, status)
