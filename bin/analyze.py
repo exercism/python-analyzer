4affbfc85694c9564ae3ee77e1110e37bdfcefc3
Trey Aguirre <tea42@cornell.edu>
2019-06-11 13:29:23 -0400

    Fixed file path generation issue

--- bin/analyze.py
+++ bin/analyze.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-file_path = os.path.join(sys.argv[2], ' {}.py'.format(sys.argv[1]))
+
+file_path = os.path.join(sys.argv[2], ' {}.py'.format(sys.argv[1].replace('-', '_')))
 
 # Add path to import analyzer
 sys.path.append('../lib/{}'.format(sys.argv[1]))
