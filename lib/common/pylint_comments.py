f13314fd602cfe77f65c41c84c9db3ad2f428fad
BethanyG <bethanymgarcia@gmail.com>
2021-04-13 00:45:01 -0700

    Functions to create and process pylint comments and reports.

---
+++ lib/common/pylint_comments.py
@@ -0,0 +1,39 @@
+from common.comment import Comment, CommentTypes
+from pylint import epylint as lint
+from pathlib import Path
+
+
+def generate_pylint_comments(in_path):
+    '''
+        Use Pylint to generate additional feedback comments for code,
+
+        e.g. if code follows PEP8 Style Convention
+    '''
+
+    pylint_stdout, _ = lint.py_run(
+        str(in_path) + ' --score=no --msg-template="{category}, {line}, {msg_id} {symbol}, {msg}"',
+        return_std=True
+    )
+
+    status_mapping = {
+        'informational': CommentTypes.INFORMATIVE,
+        'refactor' :  CommentTypes.ACTIONABLE,
+        'convention' : CommentTypes.ACTIONABLE,
+        'warning' : CommentTypes.ESSENTIAL,
+        'error' : CommentTypes.ESSENTIAL,
+        'fatal' : CommentTypes.ESSENTIAL
+    }
+
+    cleaned_pylint_output = [tuple(item.strip('" ').split(', '))
+                             for item in pylint_stdout.getvalue().splitlines()
+                             if '**' not in item]
+
+    pylint_comments = []
+
+    for line in cleaned_pylint_output:
+        if line[0]:
+            pylint_comments.append(Comment(type=status_mapping[line[0]],
+                                           params={'lineno': line[1], 'code': line[2], 'message': line[3]},
+                                           comment=f'python.pylint.generic.{line[0]}'))
+
+    return pylint_comments
