e11b593d6c09f3c8c1a622f31bc6a2c825dae7fd
BethanyG <bethanymgarcia@gmail.com>
2021-04-14 14:50:57 -0700

    Refactored tests to match renamed and re-arranged code.

--- lib/common/testing.py
+++ lib/common/testing.py
@@ -5,9 +5,11 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Optional
-from .exercise import Exercise
-from .comment import Summary
+from .analysis import Analysis
+from .comment import BaseFeedback, Summary
+from .exercise import Exercise, ExerciseError
 
+BAD_CODE = "fed test();"
 
 class BaseExerciseTest(ABC):
     @property
@@ -36,9 +38,9 @@ class BaseExerciseTest(ABC):
         """
         Basic test setup.
         """
-        self.exercise = Exercise.factory(
-            self.slug, Path(self.root.name).resolve(strict=True)
-        )
+        self.exercise = Exercise.factory(self.slug,
+                                         Path(self.root.name).resolve(strict=True),
+                                         Path(self.root.name).resolve(strict=True))
         self.analyzer = self.exercise.analyzer
         self.comments = self.exercise.comments
 
@@ -46,14 +48,18 @@ class BaseExerciseTest(ABC):
         """
         Write the given content to the test file in the working area.
         """
-        self.exercise.path.write_text(content.strip())
+
+        #add newline to content to keep pylint happy
+        clean_content = f"{content.strip()}\n"
+
+        self.exercise.out_path.write_text(clean_content)
 
     def clean_test_file(self):
         """
-        Clean up the test file.
+        Clean up the test file.c
         """
-        if self.exercise.path.exists():
-            self.exercise.path.unlink()
+        if self.exercise.out_path.exists():
+            self.exercise.out_path.unlink()
 
     def get_analysis(self, content: Optional[str]):
         """
@@ -77,27 +83,28 @@ class BaseExerciseTest(ABC):
         Ensure the analyzer can handle a missing module.
         """
         analysis = self.get_analysis(None)
-        self.assertIs(analysis.status, Summary.REQUIRE)
-        self.assertIn(self.comments.NO_MODULE, analysis.comment)
+        self.assertIs(analysis.summary, Summary.REQUIRE)
+        self.assertIn(self.comments.MALFORMED_CODE,  [item.comment for item in analysis['comments']])
 
     def test_has_module(self):
         """
         Ensure the analyzer does not label an existing module as missing.
         """
         analysis = self.get_analysis("pass")
-        self.assertNotIn(self.comments.NO_MODULE, analysis.comment)
+        self.assertNotIn(self.comments.NO_MODULE,  [item.comment for item in analysis['comments']])
 
     def test_has_malformed_code(self):
         """
         Ensure the analyzer can handle malformed code.
         """
-        analysis = self.get_analysis("fed test();")
-        self.assertIs(analysis.status, Summary.REQUIRE)
-        self.assertIn(self.comments.MALFORMED_CODE, analysis.comment)
+        analysis = self.get_analysis(BAD_CODE)
+        print(analysis)
+        self.assertIs(analysis.summary, Summary.REQUIRE)
+        self.assertIn(self.comments.MALFORMED_CODE,  [item.comment for item in analysis['comments']])
 
     def test_no_malformed_code(self):
         """
         Ensure the analyzer does not mark working code as malformed.
         """
         analysis = self.get_analysis("pass")
-        self.assertNotIn(self.comments.MALFORMED_CODE, analysis.comment)
+        self.assertNotIn(self.comments.MALFORMED_CODE,  [item.comment for item in analysis['comments']])
