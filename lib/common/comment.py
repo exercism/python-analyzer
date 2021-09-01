10e6ffb0427ba24cf5d92f61dc401be6bee64f29
BethanyG <BethanyG@users.noreply.github.com>
2021-09-01 03:32:39 -0700

    Changed analyzer Summary Comments to be Title case instead of Lower (#39)

--- lib/common/comment.py
+++ lib/common/comment.py
@@ -58,14 +58,14 @@ class Summary(Enum):
     Summary of the comments for the exercise under analysis.
     """
 
-    CELEBRATE = "Congratulations! This solution is very close to ideal! We don't have any specific recommendations."
+    CELEBRATE = "🎉 Congratulations! This solution is very close to ideal! We don't have any specific recommendations."
     REQUIRE = "There are a few changes we'd like you to make before completing this exercise."
     DIRECT = "There are a few changes we suggest that can bring your solution closer to ideal."
-    INFORM = "Good work! Here are some general recommendations for improving your Python code."
+    INFORM = "Good work! 🌟 Here are some general recommendations for improving your Python code."
     GENERIC = "We don't have a custom analysis for this exercise yet, but here are some comments from PyLint to help you improve your code."
 
     def __str__(self):
-        return self.value.lower()
+        return self.value
 
     def __repr__(self):
         return f"{self.__class__.__name__}.{self.name}"
